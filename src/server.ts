f4404af4544792ad13d57289cbcef406ff313742
istiak <istiak@gmail.com>
2023-05-30 16:34:05 +0600

    check

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-const port: number = 5000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Database is connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected successfully')
     app.listen(config.port, () =>
       console.log(`Application listening on port ${config.port}`)
-    );
+    )
   } catch (error) {
-    console.log("Failed to connect database", error);
+    console.log('Failed to connect database', error)
   }
 }
 
-main();
+main()
